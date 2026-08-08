06dcb7aabc11c32afef9b74c7c157dc2e5d8a78a
alexmiller63 <46362648+alexmiller63@users.noreply.github.com>
2026-08-08 15:16:41 -0700

    Update channel_dial.adb

--- src/channel_dial.adb
+++ src/channel_dial.adb
@@ -2,7 +2,29 @@ with Ada.Text_IO;
 
 use Ada.Text_IO;
 
+with Ada.Strings.Unbounded;
+
+use Ada.Strings.Unbounded;
+
 procedure Channel_Dial is
+
+   type Channel_Record is record
+
+      Channel_Name       : Unbounded_String;
+
+      Platform           : Unbounded_String;
+
+      Topic              : Unbounded_String;
+
+      Language           : Unbounded_String;
+
+      Region             : Unbounded_String;
+
+      Description        : Unbounded_String;
+
+      Date_Last_Verified : Unbounded_String;
+
+   end record;
 
 begin
 
